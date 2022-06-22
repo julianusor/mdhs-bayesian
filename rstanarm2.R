28081ee0d270afc4180099b0d6c8aec89ef0557f
julian4u0 <jazulado1337@hotmail.com>
2022-06-22 12:13:09 -0500

    modelos y datos para cargar

--- rstanarm2.R
+++ rstanarm2.R
@@ -4,11 +4,23 @@ library(mortDHS)
 library(haven)
 library(tidyverse)
 library(rstanarm)
+library(bayesplot)
+library(cowplot)
+#save.image("~/Documents/Bayesiana/FinalProjectBayesian/dat.RData")
 
-data_siblings <- read_dhs_surv("data/descomprimir/rwanda-2020.dta", n_max = 300)
+data_1 <- read_dhs_surv("data/descomprimir/rwanda-2020.dta", n_max = 400)
+data_2 <- read_dhs_surv("data/descomprimir/malawi-2015-16.dta", n_max = 400)
+data_3 <- read_dhs_surv("data/descomprimir/senegal-2017.dta", n_max = 400)
 
-# 71901 rows
-#data_siblings <- read_dhs_surv("data/descomprimir/rwanda-2020.dta")
+data_1$country <- "rw"
+data_2$country <- "ma"
+data_3$country <- "se"
+
+data_siblings <- bind_rows(data_1, data_2)
+data_siblings <- bind_rows(data_siblings, data_3)
+data_siblings <- bind_rows(data_siblings, data_4)
+rm(data_1, data_2, data_3)
+
 
 
 # Los datos vienen en el formato 
@@ -18,61 +30,142 @@ data_siblings <- read_dhs_surv("data/descomprimir/rwanda-2020.dta", n_max = 300)
 # 0 = vivo (censurado)
 # 1 = muerto
 
-# formato
-#1       male
-#2     female
+
+
+
+# born from 1960 to 1980
+
+year <- 1960
+n <- 20
+start <- (year - 1900) * 12 + 0
+finish <- (year - 1900) * 12 + 12 * n
+
+condition <-
+  (data_siblings$birth_cmc < finish) &
+  (data_siblings$birth_cmc > start)
+data_siblings <- data_siblings[condition, ]
 
 data_siblings$sex <- data_siblings$sex %>% as.factor()
+data_siblings$country <- data_siblings$country %>% as.factor()
 
-data_siblings$age <- data_siblings$death_time / 12
-data_siblings$death_time < 18*12
 # ceros a 0.1
 data_siblings <- data_siblings %>%
   mutate(death_time = if_else(death_time == 0, 0.1, death_time))
-#
-data_siblings <- data_siblings %>%
-  mutate(age_group = if_else(death_time < 18*12, "menor", "mayor"))
 
 # =========== modelo 1.
 qlist <- quantile(data_siblings$death_time, seq(0.05, 0.95, length.out = 4), na.rm = TRUE)
-(qlist)
 qlist <- as.vector(qlist)
-(qlist)
-mod1 <-
+
+mod_spline <-
   stan_surv(
-    formula = Surv(death_time, survival_status) ~ -1 + sex + age_group,
+    formula = Surv(death_time, survival_status) ~ sex + country,
     data = data_siblings,
     basehaz = "ms" ,
     basehaz_ops = list(degree = 3, knots = qlist),
-    #prior_aux = dirichlet(2),
     iter = 2000
   )
 
+mod_exp <-
+  stan_surv(
+    formula = Surv(death_time, survival_status) ~ sex + country,
+    data = data_siblings,
+    basehaz = "exp"
+  )
+
+mod_weibull <-
+  stan_surv(
+    formula = Surv(death_time, survival_status) ~ sex + country,
+    data = data_siblings,
+    basehaz = "weibull"
+  )
+
 summary(mod1, digits = 3)
+summary(mod2, digits = 3)
+
+#comparar
+loo_compare(loo(mod_spline), loo(mod_exp), loo(mod_weibull))
 
 # estos modelos no son tan suaves comparados al modelo no parametrico
 
-mod2 <- stan_surv(formula = Surv(death_time, survival_status) ~ -1 + sex + age_group,
-                  data = data_siblings, basehaz="exp")
+p1=plot(posterior_survfit(mod1, newdata=data.frame(sex="1", country = "rw")))
+p2=plot(posterior_survfit(mod1, newdata=data.frame(sex="2", country = "rw")))
+p3=plot(posterior_survfit(mod1, newdata=data.frame(sex="1", country = "se")))
+p4=plot(posterior_survfit(mod1, newdata=data.frame(sex="2", country = "se")))
+p5=plot(posterior_survfit(mod1, newdata=data.frame(sex="1", country = "ma")))
+p6=plot(posterior_survfit(mod1, newdata=data.frame(sex="2", country = "ma")))
+p7=plot(posterior_survfit(mod1, newdata=data.frame(sex="1", country = "co")))
+p8=plot(posterior_survfit(mod1, newdata=data.frame(sex="2", country = "co")))
 
-mod3 <- stan_surv(formula = Surv(death_time, survival_status) ~ -1 + sex + age_group,
-                  data = data_siblings, basdehaz="weibull")
+plot_grid(p1,
+          p2,
+          p3,
+          p4,
+          p5,
+          p6,
+          p7, 
+          p8,
+          ncol = 2)
 
 
+# exp
+p1=plot(posterior_survfit(mod2, newdata=data.frame(sex="1", country = "rw")))
+p2=plot(posterior_survfit(mod2, newdata=data.frame(sex="2", country = "rw")))
+p3=plot(posterior_survfit(mod2, newdata=data.frame(sex="1", country = "se")))
+p4=plot(posterior_survfit(mod2, newdata=data.frame(sex="2", country = "se")))
+p5=plot(posterior_survfit(mod2, newdata=data.frame(sex="1", country = "ma")))
+p6=plot(posterior_survfit(mod2, newdata=data.frame(sex="2", country = "ma")))
+p7=plot(posterior_survfit(mod2, newdata=data.frame(sex="1", country = "co")))
+p8=plot(posterior_survfit(mod2, newdata=data.frame(sex="2", country = "co")))
 
-bayesplot::color_scheme_set("red")
+plot_grid(p1,
+          p2,
+          p3,
+          p4,
+          p5,
+          p6,
+          p7, 
+          p8,
+          ncol = 2)
+
+# weibull
+plot_grid(p1,
+          p2,
+          p3,
+          p4,
+          p5,
+          p6,
+          p7, 
+          p8,
+          ncol = 2)
+
+
+# exp
+p1=plot(posterior_survfit(mod_weibull, newdata=data.frame(sex="1", country = "rw")))
+p2=plot(posterior_survfit(mod_weibull, newdata=data.frame(sex="2", country = "rw")))
+p3=plot(posterior_survfit(mod_weibull, newdata=data.frame(sex="1", country = "se")))
+p4=plot(posterior_survfit(mod_weibull, newdata=data.frame(sex="2", country = "se")))
+p5=plot(posterior_survfit(mod_weibull, newdata=data.frame(sex="1", country = "ma")))
+p6=plot(posterior_survfit(mod_weibull, newdata=data.frame(sex="2", country = "ma")))
+
+plot_grid(p1,
+          p2,
+          p3,
+          p4,
+          p5,
+          p6,
+          ncol = 2)
+
+color_scheme_set("red")
 
 plot(mod1, plotfun = "basehaz")
-plot_grid(mod1,  ncol = 1)
-# muy util 
+
 prior_summary(mod1) #-> prioris
 
-
 # area para coeficientes de splines
-bayesplot::mcmc_areas(mod1, regex_pars = "m-sp*", prob = 0.95)
+mcmc_areas(mod1, regex_pars = "m-sp*", prob = 0.90, prob_outer = 0.95)
 
-# area para coeficientes de splines
-bayesplot::mcmc_areas(mod1, regex_pars = "*Intercept*|sex*", prob = 0.95)
+# area para todos
+mcmc_areas(mod1, regex_pars = "(Intercept)|country*", prob = 0.90, prob_outer = 0.95)
 # como sex2 tiene un valor negativo pero cerca de 0 significa que las mujeres 
 # tienen una mortalidad mayor (la curva decrece mas rapido)
 # pero el grafico de abajo me contradice
@@ -80,15 +173,29 @@ bayesplot::mcmc_areas(mod1, regex_pars = "*Intercept*|sex*", prob = 0.95)
 # pag 18
 #"pag 24"
 
-nd <- data.frame(sex = c("1", "2"), age_group = "menor")
+nd1 <- data.frame(sex = "1", country = c("rw", "ma", "se", "co"))
+nd2 <- data.frame(sex = "2", country = c("rw", "ma", "se", "co"))
+
 posterior_survfit(
   mod1,
-  newdata = nd,
+  newdata = nd1,
   times = 0,
-  last_time = 100,
   prob = 0.95,
   extrapolate = TRUE
 ) -> pf1
+
+posterior_survfit(
+  mod1,
+  newdata = nd2,
+  times = 0,
+  prob = 0.95,
+  extrapolate = TRUE
+) -> pf2
+
+pf1 %>% filter(time < 810 & time > 800)
+
+plot(pf1)
+
 
 nd <- data.frame(sex = c("1", "2"), age_group = "mayor")
 posterior_survfit(
@@ -138,20 +245,20 @@ waic(mod3)
 
 
 compare_models(loo(mod1),
-               loo(mod2),
-               loo(mod3))
+               loo(mod2))
 
 
 
 
-p1=plot(posterior_survfit(mod1, newdata=data.frame(sex="1", age_group="1")))
-p2=plot(posterior_survfit(mod1, newdata=data.frame(sex="1", age_group="2")))
-p3=plot(posterior_survfit(mod1, newdata=data.frame(sex="1", age_group="3")))
-p4=plot(posterior_survfit(mod1, newdata=data.frame(sex="1", age_group="4")))
-p5=plot(posterior_survfit(mod1, newdata=data.frame(sex="2", age_group="1")))
-p6=plot(posterior_survfit(mod1, newdata=data.frame(sex="2", age_group="2")))
-p7=plot(posterior_survfit(mod1, newdata=data.frame(sex="2", age_group="3")))
-p8=plot(posterior_survfit(mod1, newdata=data.frame(sex="2", age_group="4")))
+p1=plot(posterior_survfit(mod1, newdata=data.frame(sex="1", age_group="menor", country = "rw")))
+p2=plot(posterior_survfit(mod1, newdata=data.frame(sex="1", age_group="menor", country = "ml")))
+p3=plot(posterior_survfit(mod1, newdata=data.frame(sex="2", age_group="menor", country = "rw")))
+p4=plot(posterior_survfit(mod1, newdata=data.frame(sex="2", age_group="menor", country = "ml")))
+
+p5=plot(posterior_survfit(mod1, newdata=data.frame(sex="1", age_group="mayor", country = "rw")))
+p6=plot(posterior_survfit(mod1, newdata=data.frame(sex="1", age_group="mayor", country = "ml")))
+p7=plot(posterior_survfit(mod1, newdata=data.frame(sex="2", age_group="mayor", country = "rw")))
+p8=plot(posterior_survfit(mod1, newdata=data.frame(sex="2", age_group="mayor", country = "ml")))
 
 library(cowplot)
 p_combined2 <- plot_grid(p1,
@@ -162,7 +269,7 @@ p_combined2 <- plot_grid(p1,
                         p6,
                         p7,
                         p8,
-                        ncol = 3)
+                        ncol = 2)
 p_combined2
 
 
@@ -186,7 +293,7 @@ library("survminer")
 # para la funcion Surv 
 # survival status = 0 vivo
 # survival status = 1 muerto
-res.cox <- coxph(Surv(death_time, survival_status) ~ sex , data =  data_siblings)
+res.cox <- coxph(Surv(death_time, survival_status) ~ sex + country, data =  data_siblings)
 
 
 summary(res.cox)
